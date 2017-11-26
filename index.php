000b44f8b8a8edb210faf6f98dda49674d2e917d
郑银彬 <1455519909@qq.com>
2017-11-26 19:33:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './phpmailer/src/SMTP.php';
+require './phpmailer/src/PHPMailer.php';
+require './phpmailer/src/Exception.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.163.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'zzzyinbin@163.com';                // SMTP username
+    $mail->Password = '123qweasd';                        // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('zzzyinbin@163.com', 'Mailer');
+    $mail->addAddress('1455519909@qq.com');               // Name is optional
+    $mail->addReplyTo('1455519909@qq.com', 'Information');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
